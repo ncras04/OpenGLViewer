01bd6f11b09368f29800b6e28575fd4b58b7a64b
ncras04 <nicolasaschmitt@googlemail.com>
2024-04-04 14:23:07 +0200

    final first lesson

--- OpenGLViewer/Application.cpp
+++ OpenGLViewer/Application.cpp
@@ -1,9 +1,48 @@
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
-int main() {
+void framebuffersize_callback(GLFWwindow* _window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
-	glfwInit();
+int main(void)
+{
+    GLFWwindow* window;
 
-	return 0;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glfwSetFramebufferSizeCallback(window, framebuffersize_callback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
