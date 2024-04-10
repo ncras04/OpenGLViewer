bc474eda14d36092476c0dca5815faed3dbaf4fc
ncras04 <nicolasaschmitt@googlemail.com>
2024-04-10 23:37:15 +0200

    go!go!

--- OpenGLViewer/Viewport.cpp
+++ OpenGLViewer/Viewport.cpp
@@ -31,27 +31,6 @@ int SViewport::Initialize(void)
 		return -2;
 	}
 
-	//unsigned int vertexShader{};
-	//vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-	//glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	//glCompileShader(vertexShader);
-
-	//unsigned int fragmentShader{};
-	//fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	//glShaderSource(fragmentShader, 1, &fragShaderSource, NULL);
-	//glCompileShader(fragmentShader);
-
-	//shaderProgram = glCreateProgram();
-
-	//glAttachShader(shaderProgram, vertexShader);
-	//glAttachShader(shaderProgram, fragmentShader);
-	//glLinkProgram(shaderProgram);
-
-	//glDeleteShader(vertexShader);
-	//glDeleteShader(fragmentShader);
-
 	return 0;
 }
 
@@ -70,7 +49,6 @@ int SViewport::Draw(void)
 
 int SViewport::LateDraw(void)
 {
-
 	glfwSwapBuffers(m_pWindow);
 	return 0;
 }
