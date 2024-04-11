ec4405f8df6361ff0a667dbe9e50b614ffcf9c2b
ncras04 <nicolasaschmitt@googlemail.com>
2024-04-11 18:54:06 +0200

    ready2

--- OpenGLViewer/Camera.cpp
+++ OpenGLViewer/Camera.cpp
@@ -10,7 +10,6 @@ void SCamera::Init()
 	view = glm::lookAt(position, position + direction, up);
 
 	projection = glm::perspective(fov, float(800 / 600), nearPlane, farPlane);
-	//projection = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.f);
 }
 
 void SCamera::Update()
